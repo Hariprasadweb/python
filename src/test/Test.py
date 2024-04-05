8a0186aba8e2c7128456129e8dbf5a7ac18d87cd
curdbackend <harijava78@gmail.com>
2024-04-05 18:09:53 +0530

    first python commit

---
+++ src/test/Test.py
@@ -0,0 +1,19 @@
+
+print("Hello world");
+
+def printer(x):
+    print("hello world "+x)
+
+def greetings(x):
+    print("First " + x + " program")
+
+
+
+printer("python")
+greetings("python")
+
+
+
+
+
+
